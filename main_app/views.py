c693c8cc7704e3e291db1f6b72f7c838a8b404aa
Hopes-Chantel <Hopes.Chantel@gmail.com>
2022-04-26 14:28:00 -0400

    create, update, and delete

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView
 from.models import Dog
 
 # Add the following import
@@ -20,3 +22,17 @@ def dogs_index(request):
 def dogs_detail(request, dog_id):
   dog = Dog.objects.get(id=dog_id)
   return render(request, 'dogs/detail.html', { 'dog': dog })
+
+class DogCreate(CreateView):
+  model = Dog
+  fields = '__all__'
+  success_url = '/dogs/'
+
+class DogUpdate(UpdateView):
+  model = Dog
+  # Let's disallow the renaming of a cat by excluding the name field!
+  fields = ['breed', 'description', 'age']
+
+class DogDelete(DeleteView):
+  model = Dog
+  success_url = '/dogs/'
