13d32b96cab5b8e12d985cbe401cbe9c4dbff6ac
Hopes-Chantel <Hopes.Chantel@gmail.com>
2022-04-26 19:38:41 -0400

    adds one to many

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,9 @@
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.edit import CreateView
 from.models import Dog
+from .forms import FeedingForm
 
 # Add the following import
 from django.http import HttpResponse
@@ -21,7 +22,19 @@ def dogs_index(request):
 
 def dogs_detail(request, dog_id):
   dog = Dog.objects.get(id=dog_id)
-  return render(request, 'dogs/detail.html', { 'dog': dog })
+  feeding_form = FeedingForm()
+  return render(request, 'dogs/detail.html', { 'dog': dog, 'feeding_form': feeding_form })
+
+def add_feeding(request, dog_id):
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # don't save the form to the db until it
+    
+    new_feeding = form.save(commit=False)
+    new_feeding.dog_id = dog_id
+    new_feeding.save()
+  return redirect('detail', dog_id=dog_id)
 
 class DogCreate(CreateView):
   model = Dog
@@ -30,7 +43,7 @@ class DogCreate(CreateView):
 
 class DogUpdate(UpdateView):
   model = Dog
-  # Let's disallow the renaming of a cat by excluding the name field!
+  
   fields = ['breed', 'description', 'age']
 
 class DogDelete(DeleteView):
