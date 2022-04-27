9d0ef5d81eff35bd9554b14046591ee6273a18c7
Hopes-Chantel <Hopes.Chantel@gmail.com>
2022-04-27 18:04:49 -0400

    adds many to many

--- main_app/views.py
+++ main_app/views.py
@@ -1,29 +1,48 @@
 
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.views.generic.edit import CreateView
-from.models import Dog
+from django.views.generic import ListView, DetailView
+from.models import Dog, Toy
 from .forms import FeedingForm
 
-# Add the following import
-from django.http import HttpResponse
 
 
-# Define the home view
+
+class DogCreate(CreateView):
+  model = Dog
+  fields = ['name', 'breed', 'description', 'age']
+  success_url = '/dogs/'
+
+class DogUpdate(UpdateView):
+  model = Dog
+  
+  fields = ['breed', 'description', 'age']
+
+class DogDelete(DeleteView):
+  model = Dog
+  success_url = '/dogs/'
+
+  # Define the home view
 def home(request):
-  return HttpResponse('<h1>Hello Doggos</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def dogs_index(request):
-  dog = Dog.objects.all()
-  return render(request, 'dogs/index.html', { 'dogs': dog })
+  dogs = Dog.objects.all()
+  return render(request, 'dogs/index.html', { 'dogs': dogs })
 
 def dogs_detail(request, dog_id):
   dog = Dog.objects.get(id=dog_id)
+  toys_dog_doesnt_have = Toy.objects.exclude(id__in = dog.toys.all().values_list('id'))
+  print(toys_dog_doesnt_have.all)
   feeding_form = FeedingForm()
-  return render(request, 'dogs/detail.html', { 'dog': dog, 'feeding_form': feeding_form })
+  return render(request, 'dogs/detail.html',  {
+  'dog': dog,
+  'feeding_form': feeding_form,
+  'toys': toys_dog_doesnt_have
+  })
 
 def add_feeding(request, dog_id):
   form = FeedingForm(request.POST)
@@ -36,16 +55,30 @@ def add_feeding(request, dog_id):
     new_feeding.save()
   return redirect('detail', dog_id=dog_id)
 
-class DogCreate(CreateView):
-  model = Dog
+
+def assoc_toy(request, dog_id, toy_id):
+  Dog.objects.get(id=dog_id).toys.add(toy_id)
+  return redirect('detail', dog_id=dog_id)
+
+
+def unassoc_toy(request, dog_id, toy_id):
+  Dog.objects.get(id=dog_id).toys.remove(toy_id)
+  return redirect('detail', dog_id=dog_id)
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
   fields = '__all__'
-  success_url = '/dogs/'
 
-class DogUpdate(UpdateView):
-  model = Dog
-  
-  fields = ['breed', 'description', 'age']
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
 
-class DogDelete(DeleteView):
-  model = Dog
-  success_url = '/dogs/'
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
